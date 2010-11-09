d275a1396a7c060c6d4effdb57bf3f92f947f347
dasenov <dasenov@5430185d-de90-4815-bc66-9e913ca9c050>
2010-11-09 12:46:14 +0000

    Share project "SelfTest" into "https://svn.inf.ethz.ch/svn/pmueller/pm-group/students/dimitar_asenov"

---
+++ sefltest/headers/TestManager.h
@@ -0,0 +1,24 @@
+/***********************************************************************************************************************
+ * TestManager.h
+ *
+ *  Created on: Nov 9, 2010
+ *      Author: Dimitar Asenov
+ **********************************************************************************************************************/
+
+#ifndef TESTMANAGER_H_
+#define TESTMANAGER_H_
+
+#include "selftest_api.h"
+
+namespace SelfTest {
+
+class SELFTEST_API TestManager
+{
+	public:
+		TestManager();
+		virtual ~TestManager();
+};
+
+}
+
+#endif /* TESTMANAGER_H_ */
---
+++ sefltest/src/TestManager.cpp
@@ -0,0 +1,23 @@
+/***********************************************************************************************************************
+ * TestManager.cpp
+ *
+ *  Created on: Nov 9, 2010
+ *      Author: Dimitar Asenov
+ **********************************************************************************************************************/
+
+#include "TestManager.h"
+
+namespace SelfTest {
+
+TestManager::TestManager()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+TestManager::~TestManager()
+{
+	// TODO Auto-generated destructor stub
+}
+
+}
