f0b1d1435bcee039734c5a95e0bc709fd764f6c3
dasenov <dasenov@5430185d-de90-4815-bc66-9e913ca9c050>
2010-11-29 11:11:24 +0000

    Added a test for the single unit write mechanism

--- modelbase/test/ConcurrencyTests.cpp
+++ modelbase/test/ConcurrencyTests.cpp
@@ -16,21 +16,61 @@
 
 namespace Model {
 
-TEST(ModelBase, ConcurrentRead)
+TEST(ModelBase, SingleWriteUnitCheck)
 {
 	Model model;
 
 	BinaryNode* root = dynamic_cast<BinaryNode*> (model.createRoot("BinaryNode"));
 
 	model.beginModification(root, "make tree");
-	root->makeLeftNode();
-	root->makeRightNode();
-	root->text()->set("Troot");
-	root->left()->text()->set("Tleft");
-	root->right()->text()->set("Tright");
+	BinaryNode* left = root->makeLeftNode();
+	BinaryNode* right = root->makeRightNode("BinaryNodeUnit");
+	BinaryNode* one = root->left()->makeLeftNode("BinaryNodeUnit");
+	BinaryNode* two = root->left()->makeRightNode("BinaryNodeUnit");
 	model.endModification();
-	CHECK_INT_EQUAL(0,0)
 
+	CHECK_STR_EQUAL(QString(), root->text()->get());
+	CHECK_STR_EQUAL(QString(), left->text()->get());
+	CHECK_STR_EQUAL(QString(), right->text()->get());
+	CHECK_STR_EQUAL(QString(), one->text()->get());
+	CHECK_STR_EQUAL(QString(), two->text()->get());
+
+	model.beginModification(root, "Modify root");
+
+	root->text()->set("This is ok");
+	CHECK_STR_EQUAL("This is ok", root->text()->get());
+
+	CHECK_FOR_EXCEPTION(ModelException, one->text()->set("This should fail"));
+	CHECK_STR_EQUAL(QString(), one->text()->get());
+
+	model.changeModificationTarget(one);
+	CHECK_FOR_EXCEPTION(ModelException, root->text()->set("This should fail"));
+	CHECK_STR_EQUAL("This is ok", root->text()->get());
+
+	one->text()->set("one set");
+	CHECK_STR_EQUAL("one set", one->text()->get());
+
+	model.endModification();
+
+	model.beginModification(NULL);
+	model.undo();
+	model.endModification();
+
+	CHECK_STR_EQUAL(QString(), root->text()->get());
+	CHECK_STR_EQUAL(QString(), left->text()->get());
+	CHECK_STR_EQUAL(QString(), right->text()->get());
+	CHECK_STR_EQUAL(QString(), one->text()->get());
+	CHECK_STR_EQUAL(QString(), two->text()->get());
+
+	model.beginModification(NULL);
+	model.redo();
+	model.endModification();
+
+	CHECK_STR_EQUAL("This is ok", root->text()->get());
+	CHECK_STR_EQUAL(QString(), left->text()->get());
+	CHECK_STR_EQUAL(QString(), right->text()->get());
+	CHECK_STR_EQUAL("one set", one->text()->get());
+	CHECK_STR_EQUAL(QString(), two->text()->get());
 }
 
 }
