70b85acd26dba684b5622966920f00ac01ff3755
dasenov <dasenov@5430185d-de90-4815-bc66-9e913ca9c050>
2010-12-03 17:57:04 +0000

    Added a check whether two text files have the same content.

--- SelfTest/headers/TestAssertions.h
+++ SelfTest/headers/TestAssertions.h
@@ -70,6 +70,54 @@
 /**********************************************************************************************************************/
 
 /**********************************************************************************************************************/
+#define CHECK_TEXT_FILES_EQUAL(expectedFileName, actualFileName)																		\
+	try																																					\
+	{																																						\
+		QFile exp(expectedFileName);																												\
+		QFile act(actualFileName);																													\
+		if (exp.open(QIODevice::ReadOnly | QIODevice::Text))																				\
+		{																																					\
+			if (act.open(QIODevice::ReadOnly | QIODevice::Text))																			\
+			{																																				\
+				QTextStream inexp(&exp);																											\
+				QString strexp = inexp.readAll();																								\
+																																							\
+				QTextStream inact(&act);																											\
+				QString stract = inact.readAll();																								\
+																																							\
+				if (strexp == stract) testResults.addPassedCheck();																		\
+				else																																		\
+				{																																			\
+					allChecksPassedFlag = false;																									\
+					QString message = getName() + "\t" + __FILE__ + ":" + QString::number(__LINE__) + "\t";					\
+					message += "File " + QString(actualFileName) + " differs from the expected output.";						\
+					testResults.addFailedCheck(message);																						\
+				}																																			\
+			}																																				\
+			else																																			\
+			{																																				\
+				allChecksPassedFlag = false;																										\
+				QString message = getName() + "\t" + __FILE__ + ":" + QString::number(__LINE__) + "\t";						\
+				message += "Could not open file " + QString(actualFileName) + " for reading.";									\
+				testResults.addFailedCheck(message);																							\
+			}																																				\
+		}																																					\
+		else																																				\
+		{																																					\
+			allChecksPassedFlag = false;																											\
+			QString message = getName() + "\t" + __FILE__ + ":" + QString::number(__LINE__) + "\t";							\
+			message += "Could not open file " + QString(expectedFileName) + " for reading.";										\
+			testResults.addFailedCheck(message);																								\
+		}																																					\
+	} catch (...)																																		\
+	{																																						\
+		allChecksPassedFlag = false;																												\
+		testResults.addFailedCheck(getName() + "\t" + __FILE__ + ":" + QString::number(__LINE__) + " Uncaught exception");	\
+		throw;																																			\
+	}
+/**********************************************************************************************************************/
+
+/**********************************************************************************************************************/
 #define CHECK_CONDITION( condition )																											\
 	try																																					\
 	{																																						\
