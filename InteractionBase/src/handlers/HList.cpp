78cefd54ffd4559e659fcc3e019a7737e3bcfcff
dasenov <dasenov@5430185d-de90-4815-bc66-9e913ca9c050>
2011-01-26 12:06:10 +0000

    Implemented basic list navigation using the arrow keys.

--- InteractionBase/src/handlers/HList.cpp
+++ InteractionBase/src/handlers/HList.cpp
@@ -8,6 +8,7 @@
 #include "handlers/HList.h"
 
 #include "VisualizationBase/headers/items/VList.h"
+#include "VisualizationBase/headers/Scene.h"
 #include "FilePersistence/headers/SystemClipboard.h"
 #include "ModelBase/headers/nodes/List.h"
 
@@ -26,11 +27,12 @@ HList* HList::instance()
 
 void HList::keyPressEvent(Visualization::Item *target, QKeyEvent *event)
 {
+	Visualization::VList* list = static_cast<Visualization::VList*> (target);
+	bool processed = false;
+
 	if (event->modifiers() == (Qt::ControlModifier | Qt::ShiftModifier)  && event->key() == Qt::Key_V)
 	{
 		FilePersistence::SystemClipboard clipboard;
-
-		Visualization::VList* list = static_cast<Visualization::VList*> (target);
 		int selIndex = list->focusedElementIndex();
 
 		if (clipboard.numNodes() > 0 && selIndex >= 0)
@@ -42,8 +44,65 @@ void HList::keyPressEvent(Visualization::Item *target, QKeyEvent *event)
 			l->model()->endModification();
 			target->setUpdateNeeded();
 		}
+
+		processed = true;
 	}
-	else GenericHandler::keyPressEvent(target, event);
+	else if (event->modifiers() == 0)
+	{
+		Visualization::Item* toFocus = NULL;
+		int currentFocusIndex = list->focusedElementIndex();
+		bool first = currentFocusIndex == 0;
+		bool last = currentFocusIndex == list->length()-1;
+
+		switch( event->key() )
+		{
+			case Qt::Key_Up:
+			{
+				if (list->style()->itemsStyle().direction() == Visualization::LayoutStyle::TopToBottom && !first)
+					toFocus = list->at<Visualization::Item>(currentFocusIndex - 1);
+				if (list->style()->itemsStyle().direction() == Visualization::LayoutStyle::BottomToTop && !last)
+					toFocus = list->at<Visualization::Item>(currentFocusIndex + 1);
+			}
+			break;
+			case Qt::Key_Down:
+			{
+				if (list->style()->itemsStyle().direction() == Visualization::LayoutStyle::TopToBottom && !last)
+					toFocus = list->at<Visualization::Item>(currentFocusIndex + 1);
+				if (list->style()->itemsStyle().direction() == Visualization::LayoutStyle::BottomToTop && !first)
+					toFocus = list->at<Visualization::Item>(currentFocusIndex - 1);
+			}
+			break;
+			case Qt::Key_Left:
+			{
+				if (list->style()->itemsStyle().direction() == Visualization::LayoutStyle::LeftToRight && !first)
+					toFocus = list->at<Visualization::Item>(currentFocusIndex - 1);
+				if (list->style()->itemsStyle().direction() == Visualization::LayoutStyle::RightToLeft && !last)
+					toFocus = list->at<Visualization::Item>(currentFocusIndex + 1);
+			}
+			break;
+			case Qt::Key_Right:
+			{
+				if (list->style()->itemsStyle().direction() == Visualization::LayoutStyle::LeftToRight && !last)
+					toFocus = list->at<Visualization::Item>(currentFocusIndex + 1);
+				if (list->style()->itemsStyle().direction() == Visualization::LayoutStyle::RightToLeft && !first)
+					toFocus = list->at<Visualization::Item>(currentFocusIndex - 1);
+			}
+			break;
+		}
+
+		processed = toFocus;
+		if (toFocus)
+		{
+			target->scene()->clearSelection();
+			toFocus->setFocus();
+			toFocus->setSelected(true);
+			toFocus->setUpdateNeeded();
+			target->setUpdateNeeded();
+		}
+	}
+	else
+
+	if (!processed)GenericHandler::keyPressEvent(target, event);
 }
 
 }
