3e12b21a29951b0f965697bfbeae0bc8b745a3cb
dasenov <dasenov@5430185d-de90-4815-bc66-9e913ca9c050>
2011-02-16 12:01:10 +0000

    CHANGED: A loop's condition is now optional

--- OOModel/src/statements/LoopStatement.cpp
+++ OOModel/src/statements/LoopStatement.cpp
@@ -12,7 +12,7 @@ namespace OOModel {
 EXTENDABLENODE_DEFINE_EMPTY_CONSTRUCTORS(LoopStatement, Statement)
 EXTENDABLENODE_DEFINE_TYPE_REGISTRATION_METHODS(LoopStatement, Statement)
 
-REGISTER_ATTRIBUTE(LoopStatement, condition, Expression, false, false, true)
+REGISTER_ATTRIBUTE(LoopStatement, condition, Expression, false, true, true)
 REGISTER_ATTRIBUTE(LoopStatement, initStep, Statement, false, true, true)
 REGISTER_ATTRIBUTE(LoopStatement, updateStep, Statement, false, true, true)
 REGISTER_ATTRIBUTE(LoopStatement, body, Statement, false, false, true)
