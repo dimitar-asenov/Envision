a6d1f982d381c65c06e277f8dd5a5b4a28e48da0
dasenov <dasenov@5430185d-de90-4815-bc66-9e913ca9c050>
2011-02-16 10:49:16 +0000

    CHANGED: Reorganized the folder structure a bit.

---
+++ OOVisualization/src/top_level/VModuleStyle.cpp
@@ -0,0 +1,33 @@
+/***********************************************************************************************************************
+ * VModuleStyle.cpp
+ *
+ *  Created on: Feb 4, 2011
+ *      Author: Dimitar Asenov
+ **********************************************************************************************************************/
+
+#include "top_level/VModuleStyle.h"
+#include "VisualizationBase/headers/Styles.h"
+
+namespace OOVisualization {
+
+VModuleStyle::VModuleStyle()
+{
+}
+
+VModuleStyle* VModuleStyle::getDefault()
+{
+	static VModuleStyle defaultStyle;
+	return &defaultStyle;
+}
+
+void VModuleStyle::load()
+{
+	ItemStyle::load();
+	Visualization::Styles::load("border", border_);
+	Visualization::Styles::load("header", header_);
+	Visualization::Styles::load("name", name_);
+	Visualization::Styles::load("icon", icon_);
+	Visualization::Styles::load("content", content_);
+}
+
+}
