24591cbf3de09c933d99865cf33f2e87c536cf63
Dimitar Asenov <dimitar.asenov@inf.ethz.ch>
2012-10-10 11:09:06 +0200

    Enable core dumps when compiling a debug build in Linux

--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -36,6 +36,13 @@
 #include "DefaultEnvisionManager.h"
 #include "TestRunner.h"
 
+// Enable core dumps of debug builds on Linux
+#ifdef Q_OS_LINUX
+#ifdef DEBUG
+#include <sys/resource.h>
+#endif
+#endif
+
 using namespace Core;
 
 /**
@@ -44,37 +51,49 @@ using namespace Core;
  */
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+	QApplication a(argc, argv);
 
-    EnvisionWindow w;
-    w.show();
+	// Enable core dumps of debug builds on Linux
+#ifdef Q_OS_LINUX
+#ifdef DEBUG
+	struct rlimit core_limit;
+	core_limit.rlim_cur = RLIM_INFINITY;
+	core_limit.rlim_max = RLIM_INFINITY;
 
-    TestRunner testr;
+	if(setrlimit(RLIMIT_CORE, &core_limit) < 0)
+		qDebug() << "Error while enabling core dumps:" << strerror(errno);
+#endif
+#endif
 
-    int retCode = 0;
-    try
-    {
-   	 PluginManager pm;
-   	 DefaultEnvisionManager manager;
+	EnvisionWindow w;
+	w.show();
 
-   	 // Give the Envision manager all the information it needs to operate properly.
-   	 manager.setPluginManager(&pm);
-   	 manager.setMainWindow(&w);
+	TestRunner testr;
 
-   	 pm.loadAllPlugins(manager);
+	int retCode = 0;
+	try
+	{
+		PluginManager pm;
+		DefaultEnvisionManager manager;
 
-   	 testr.enqueueSelfTests(pm);
+		// Give the Envision manager all the information it needs to operate properly.
+		manager.setPluginManager(&pm);
+		manager.setMainWindow(&w);
 
-   	 QTextStream out(stdout);
-   	 out << "==============================" << endl;
+		pm.loadAllPlugins(manager);
 
-   	 retCode = a.exec();
-    }
-    catch (EnvisionException &e)
-    {
-   	 e.printError();
-   	 retCode = 1;
-    }
+		testr.enqueueSelfTests(pm);
 
-    return retCode;
+		QTextStream out(stdout);
+		out << "==============================" << endl;
+
+		retCode = a.exec();
+	}
+	catch (EnvisionException &e)
+	{
+		e.printError();
+		retCode = 1;
+	}
+
+	return retCode;
 }
