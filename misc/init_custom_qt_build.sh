01917bcad224e23459156c75c384aebfb94a8208
Dimitar Asenov <dimitar.asenov@inf.ethz.ch>
2012-11-09 14:01:03 +0100

    Uncomment commands in the Qt setup script

--- misc/init_custom_qt_build.sh
+++ misc/init_custom_qt_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 WORKSPACE_DIR=$( cd $(dirname $0)/.. ; pwd )
 QT_SRC_DIR=$WORKSPACE_DIR/qt/qt-envision-enhancements
@@ -9,7 +10,7 @@ ENVISION_QT_LIB_LIND_DIR=$WORKSPACE_DIR/DebugBuild/qt
 echo "################################################################################"
 echo "Cloning Qt repository to" $QT_SRC_DIR
 echo "################################################################################"
-#git clone git://gitorious.org/~mitko/qt/qt-envision-enhancements.git $QT_SRC_DIR
+git clone git://gitorious.org/~mitko/qt/qt-envision-enhancements.git $QT_SRC_DIR
 
 cd $QT_SRC_DIR
 
@@ -17,17 +18,17 @@ echo "##########################################################################
 echo "Configuring Qt Build in" $QT_SRC_DIR
 echo "Build prefix is" $QT_BUILD_DIR
 echo "################################################################################"
-#./configure -prefix $QT_BUILD_DIR -release -opensource -no-webkit -no-javascript-jit -no-gtkstyle -nomake examples -nomake demos
+./configure -prefix $QT_BUILD_DIR -release -opensource -no-webkit -no-javascript-jit -no-gtkstyle -nomake examples -nomake demos
 
 echo "################################################################################"
 echo "Compiling Qt at" $QT_SRC_DIR
 echo "################################################################################"
-#make -j8 | grep "error:"
+make -j8 | grep "error:"
 
 echo "################################################################################"
 echo "Installing Qt to" $QT_BUILD_DIR
 echo "################################################################################"
-#make install
+make install
 
 echo "################################################################################"
 echo "Creating symbolic link" $ENVISION_QT_LIB_LIND_DIR
