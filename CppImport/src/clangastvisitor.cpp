bf374293f843ebd7943f76d4057db004100d2635
Lukas Vogel <lukedirtwalker@gmail.com>
2013-02-28 14:04:53 +0000

    began impl of clangastvisitor

--- CppImport/src/clangastvisitor.cpp
+++ CppImport/src/clangastvisitor.cpp
@@ -1,5 +1,28 @@
 #include "clangastvisitor.h"
 
-ClangAstVisitor::ClangAstVisitor()
+
+
+using namespace clang;
+using namespace llvm;
+
+bool ClangAstVisitor::VisitStmt(clang::Stmt *S)
 {
+    if(isa<IfStmt>(S))
+    {
+
+    }
+    return true;
+}
+
+
+
+bool ClangAstVisitor::VisitDecl(clang::Decl *D)
+{
+    if(isa<RecordDecl>(D))
+    {
+//        RecordDecl *recDecl = cast<RecordDecl>(D);
+
+    }
+
+    return true;
 }
--- CppImport/src/clangastvisitor.h
+++ CppImport/src/clangastvisitor.h
@@ -1,11 +1,40 @@
 #ifndef CLANGASTVISITOR_H
 #define CLANGASTVISITOR_H
 
-#include "clang/AST/RecursiveASTVisitor.h"
 
-class ClangAstVisitor : public clang::RecursiveASTVisitor
+//CLANG
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/DeclCXX.h"
+#include "clang/AST/Decl.h"
+#include "clang/AST/Stmt.h"
+#include "clang/AST/Type.h"
+
+
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/FileManager.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Basic/TargetOptions.h"
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Parse/ParseAST.h"
+//#include "clang/Rewrite/Rewriter.h"
+//#include "clang/Rewrite/Rewriters.h"
+
+
+//LLVM
+#include "llvm/Support/Host.h"
+
+using clang::RecursiveASTVisitor;
+
+class ClangAstVisitor : public RecursiveASTVisitor <ClangAstVisitor>
 {
 public:
+
+    bool VisitStmt(clang::Stmt *S);
+    bool VisitDecl(clang::Decl *D);
 };
 
 #endif // CLANGASTVISITOR_H
