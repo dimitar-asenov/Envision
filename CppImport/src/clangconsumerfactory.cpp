ddfd66c71b36a834f338c6f1d97944b58fc3a38f
Lukas Vogel <lukedirtwalker@gmail.com>
2013-03-03 20:53:23 +0100

    added clang consumer factory

---
+++ CppImport/src/clangconsumerfactory.cpp
@@ -0,0 +1,11 @@
+#include "clangconsumerfactory.h"
+
+ClangConsumerFactory::ClangConsumerFactory(Model::Model *model, OOModel::Project *project)
+    : model_(model) , project_(project)
+{
+}
+
+clang::ASTConsumer *ClangConsumerFactory::newASTConsumer()
+{
+    return new ClangAstConsumer(model_,project_);
+}
---
+++ CppImport/src/clangconsumerfactory.h
@@ -0,0 +1,20 @@
+#ifndef CLANGCONSUMERFACTORY_H
+#define CLANGCONSUMERFACTORY_H
+
+#include "clangastconsumer.h"
+
+#include "OOModel/src/allOOModelNodes.h"
+#include "clang/AST/ASTConsumer.h"
+
+class ClangConsumerFactory
+{
+public:
+    ClangConsumerFactory(Model::Model* model, OOModel::Project* project);
+    clang::ASTConsumer* newASTConsumer();
+
+private:
+    Model::Model* model_;
+    OOModel::Project* project_;
+};
+
+#endif // CLANGCONSUMERFACTORY_H
