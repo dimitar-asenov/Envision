14329e45a79936d24d4d7ad157916e524c5c94ed
Lukas Vogel <lukedirtwalker@gmail.com>
2013-03-15 12:33:32 +0100

    changed order to fix boolean

--- CppImport/src/CppImportUtilities.cpp
+++ CppImport/src/CppImportUtilities.cpp
@@ -2,10 +2,10 @@
 
 OOModel::Expression *CppImportUtilities::convertClangType(clang::QualType type)
 {
-    if(type.getTypePtr()->isIntegerType())
-        return new OOModel::PrimitiveTypeExpression(OOModel::PrimitiveTypeExpression::PrimitiveTypes::INT);
-    else if(type.getTypePtr()->isBooleanType())
+    if(type.getTypePtr()->isBooleanType())
         return new OOModel::PrimitiveTypeExpression(OOModel::PrimitiveTypeExpression::PrimitiveTypes::BOOLEAN);
+    else if(type.getTypePtr()->isIntegerType())
+        return new OOModel::PrimitiveTypeExpression(OOModel::PrimitiveTypeExpression::PrimitiveTypes::INT);
     else if(type.getTypePtr()->isVoidType())
         return new OOModel::PrimitiveTypeExpression(OOModel::PrimitiveTypeExpression::PrimitiveTypes::VOID);
     else
