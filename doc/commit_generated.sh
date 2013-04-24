1b674e002fcaec960f94fc78b06a231d7468b9e8
Dimitar Asenov <dimitar.asenov@inf.ethz.ch>
2013-04-24 16:43:28 +0200

    Add a script to simplify publishing the documentation

---
+++ doc/commit_generated.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+  echo "Usage: `basename $0` \"Some commit message\""
+  exit 1
+fi
+
+cd generated
+git add -A
+git commit -m %1
+git push origin gh-pages
+cd ..
