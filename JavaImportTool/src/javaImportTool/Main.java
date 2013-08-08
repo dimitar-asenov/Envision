fb75463f5ff042714659b5bf339c305a51781563
Dimitar Asenov <dimitar.asenov@inf.ethz.ch>
2013-08-08 15:48:16 +0200

    Add an option to the JavaImportTool to parse .class files

--- JavaImportTool/src/javaImportTool/Main.java
+++ JavaImportTool/src/javaImportTool/Main.java
@@ -7,26 +7,34 @@ import java.io.PrintStream;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.eclipse.jdt.core.ToolFactory;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.util.ClassFileBytesDisassembler;
+import org.eclipse.jdt.core.util.ClassFormatException;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		if (args == null || args.length != 3)
+		if (args == null
+			|| (args.length < 3 || args.length > 4)
+			|| (args.length == 4 && !args[0].equals("--classFiles")))
 		{
 			System.out.println(usageInfo);
 			return;
 		}
 		
-		String projectName = args[0];
+		boolean classFiles = args.length == 4 ;
+		int argIndex = classFiles ? 1 : 0;
 		
-		String inputDirectory = args[1];
+		String projectName = args[argIndex + 0];
+		
+		String inputDirectory = args[argIndex + 1];
 		if (!inputDirectory.endsWith(File.separator)) inputDirectory += File.separator;
 		
-		String outputDirectory = args[2];
+		String outputDirectory = args[argIndex + 2];
 		if (!outputDirectory.endsWith(File.separator)) outputDirectory += File.separator;
 		
 		try
@@ -35,11 +43,20 @@ public class Main {
 			root.setSymbol(projectName);
 
 			File dir = new File(inputDirectory);
-			for(File file : FileUtils.listFiles(dir, new SuffixFileFilter(".java"), TrueFileFilter.INSTANCE))
+			ClassFileBytesDisassembler dis = ToolFactory.createDefaultClassFileBytesDisassembler();
+			
+			String suffix =  classFiles ? ".class" : ".java";
+			for(File file : FileUtils.listFiles(dir, new SuffixFileFilter(suffix), TrueFileFilter.INSTANCE))
 			{
 				System.out.print("Processing file: " + file.getPath() + "...");
-				String source = FileUtils.readFileToString(file);
 				ASTParser parser = ASTParser.newParser(AST.JLS4);
+				
+				String source;
+				if (classFiles)
+					source = dis.disassemble(FileUtils.readFileToByteArray(file), System.getProperty("line.separator"));
+				else
+					source = FileUtils.readFileToString(file);
+				
 				parser.setSource(source.toCharArray());
 				CompilationUnit unit = (CompilationUnit) parser.createAST(null);
 				ASTConverter tl = new ASTConverter(root, source);
@@ -59,11 +76,16 @@ public class Main {
 			e.printStackTrace();
 		} catch (ConversionException e) {
 			e.printStackTrace();
+		} catch (ClassFormatException e) {
+			e.printStackTrace();
 		}
 	}
 	
 	private static String usageInfo =
 			  "Usage: JavaImportTool project-name input-directory output-directory\n"
+		  	+ "\n"
+			+ "   [--classFiles]\n"
+			+ "      Parse .class fiels instead of .java files.\n"
 			+ "\n"
 			+ "   project-name\n"
 			+ "      This is the name of the project being converted. This will be used as a directory and file names\n"
