eff84cd53a79df6964beb5aad7efd8f65170ed3d
patrick-luethi <patrick.luethi@besonet.ch>
2014-01-28 19:18:43 +0100

    Use new way of specifying styles in VClassSzPublic

--- OOVisualization/src/semantic_zoom/VClassSzPublicStyle.h
+++ OOVisualization/src/semantic_zoom/VClassSzPublicStyle.h
@@ -39,98 +39,36 @@ namespace OOVisualization {
 
 class OOVISUALIZATION_API VClassSzPublicStyle : public Visualization::DeclarativeItemBaseStyle
 {
-	private:
-		Visualization::TextStyle nameDefault_;
-		Visualization::TextStyle namePublic_;
-		Visualization::TextStyle namePrivate_;
-		Visualization::TextStyle nameProtected_;
-
-		Visualization::StaticStyle classIcon_;
-		Visualization::StaticStyle interfaceIcon_;
-		Visualization::StaticStyle structIcon_;
-		Visualization::StaticStyle unionIcon_;
-		Visualization::StaticStyle enumIcon_;
-		Visualization::StaticStyle annotationIcon_;
-
-		Visualization::SequentialLayoutStyle header_;
-		Visualization::VListStyle typeArguments_;
-		Visualization::VListStyle baseClasses_;
-		Visualization::VListStyle annotations_;
-		Visualization::VListStyle enumerators_;
-		Visualization::StaticStyle friendsSymbol_;
-		Visualization::VListStyle friends_;
-		Visualization::VListStyle declarations_;
-		Visualization::PositionLayoutStyle body_;
-		Visualization::SequentialLayoutStyle content_;
-		Visualization::SequentialLayoutStyle fieldContainer_;
-		Visualization::SequentialLayoutStyle publicFieldArea_;
-		Visualization::SequentialLayoutStyle privateFieldArea_;
-		Visualization::SequentialLayoutStyle protectedFieldArea_;
-		Visualization::SequentialLayoutStyle defaultFieldArea_;
-
 	public:
-		void load(Visualization::StyleLoader& sl);
+		virtual ~VClassSzPublicStyle() override;
 
-		const Visualization::TextStyle& nameDefault() const;
-		const Visualization::TextStyle& namePublic() const;
-		const Visualization::TextStyle& namePrivate() const;
-		const Visualization::TextStyle& nameProtected() const;
+		Property<Visualization::TextStyle> nameDefault{this, "nameDefault"};
+		Property<Visualization::TextStyle> namePublic{this, "namePublic"};
+		Property<Visualization::TextStyle> namePrivate{this, "namePrivate"};
+		Property<Visualization::TextStyle> nameProtected{this, "nameProtected"};
 
-		const Visualization::StaticStyle& classIcon() const;
-		const Visualization::StaticStyle& interfaceIcon() const;
-		const Visualization::StaticStyle& structIcon() const;
-		const Visualization::StaticStyle& unionIcon() const;
-		const Visualization::StaticStyle& enumIcon() const;
-		const Visualization::StaticStyle& annotationIcon() const;
+		Property<Visualization::StaticStyle> classIcon{this, "classIcon"};
+		Property<Visualization::StaticStyle> interfaceIcon{this, "interfaceIcon"};
+		Property<Visualization::StaticStyle> structIcon{this, "structIcon"};
+		Property<Visualization::StaticStyle> unionIcon{this, "unionIcon"};
+		Property<Visualization::StaticStyle> enumIcon{this, "enumIcon"};
+		Property<Visualization::StaticStyle> annotationIcon{this, "annotationIcon"};
 
-		const Visualization::SequentialLayoutStyle& header() const;
-		const Visualization::VListStyle& typeArguments() const;
-		const Visualization::VListStyle& baseClasses() const;
-		const Visualization::VListStyle& annotations() const;
-		const Visualization::VListStyle& enumerators() const;
-		const Visualization::StaticStyle& friendsSymbol() const;
-		const Visualization::VListStyle& friends() const;
-		const Visualization::VListStyle& declarations() const;
-		const Visualization::PositionLayoutStyle& body() const;
-		const Visualization::SequentialLayoutStyle& content() const;
-		const Visualization::SequentialLayoutStyle& fieldContainer() const;
-		const Visualization::SequentialLayoutStyle& publicFieldArea() const;
-		const Visualization::SequentialLayoutStyle& privateFieldArea() const;
-		const Visualization::SequentialLayoutStyle& protectedFieldArea() const;
-		const Visualization::SequentialLayoutStyle& defaultFieldArea() const;
+		Property<Visualization::SequentialLayoutStyle> header{this, "header"};
+		Property<Visualization::VListStyle> typeArguments{this, "typeArguments"};
+		Property<Visualization::VListStyle> baseClasses{this, "baseClasses"};
+		Property<Visualization::VListStyle> annotations{this, "annotations"};
+		Property<Visualization::VListStyle> enumerators{this, "enumerators"};
+		Property<Visualization::StaticStyle> friendsSymbol{this, "friendsSymbol"};
+		Property<Visualization::VListStyle> friends{this, "friends"};
+		Property<Visualization::VListStyle> declarations{this, "declarations"};
+		Property<Visualization::PositionLayoutStyle> body{this, "body"};
+		Property<Visualization::SequentialLayoutStyle> content{this, "content"};
+		Property<Visualization::SequentialLayoutStyle> fieldContainer{this, "fieldContainer"};
+		Property<Visualization::SequentialLayoutStyle> publicFieldArea{this, "publicFieldArea"};
+		Property<Visualization::SequentialLayoutStyle> privateFieldArea{this, "privateFieldArea"};
+		Property<Visualization::SequentialLayoutStyle> protectedFieldArea{this, "protectedFieldArea"};
+		Property<Visualization::SequentialLayoutStyle> defaultFieldArea{this, "defaultFieldArea"};
 };
-
-inline const Visualization::TextStyle& VClassSzPublicStyle::nameDefault() const { return nameDefault_; }
-inline const Visualization::TextStyle& VClassSzPublicStyle::namePublic() const { return namePublic_; }
-inline const Visualization::TextStyle& VClassSzPublicStyle::namePrivate() const { return namePrivate_; }
-inline const Visualization::TextStyle& VClassSzPublicStyle::nameProtected() const { return nameProtected_; }
-
-inline const Visualization::StaticStyle& VClassSzPublicStyle::classIcon() const { return classIcon_; }
-inline const Visualization::StaticStyle& VClassSzPublicStyle::interfaceIcon() const { return interfaceIcon_; }
-inline const Visualization::StaticStyle& VClassSzPublicStyle::structIcon() const { return structIcon_; }
-inline const Visualization::StaticStyle& VClassSzPublicStyle::unionIcon() const { return unionIcon_; }
-inline const Visualization::StaticStyle& VClassSzPublicStyle::enumIcon() const { return enumIcon_; }
-inline const Visualization::StaticStyle& VClassSzPublicStyle::annotationIcon() const { return annotationIcon_; }
-
-inline const Visualization::SequentialLayoutStyle& VClassSzPublicStyle::header() const { return header_; }
-inline const Visualization::VListStyle& VClassSzPublicStyle::typeArguments() const { return typeArguments_; }
-inline const Visualization::VListStyle& VClassSzPublicStyle::baseClasses() const { return baseClasses_; }
-inline const Visualization::VListStyle& VClassSzPublicStyle::annotations() const { return annotations_; }
-inline const Visualization::VListStyle& VClassSzPublicStyle::enumerators() const { return enumerators_; }
-inline const Visualization::StaticStyle& VClassSzPublicStyle::friendsSymbol() const{ return friendsSymbol_; }
-inline const Visualization::VListStyle& VClassSzPublicStyle::friends() const { return friends_; }
-inline const Visualization::VListStyle& VClassSzPublicStyle::declarations() const { return declarations_; }
-inline const Visualization::PositionLayoutStyle& VClassSzPublicStyle::body() const { return body_; }
-inline const Visualization::SequentialLayoutStyle& VClassSzPublicStyle::content() const { return content_; }
-inline const Visualization::SequentialLayoutStyle& VClassSzPublicStyle::fieldContainer()
-	const { return fieldContainer_; }
-inline const Visualization::SequentialLayoutStyle& VClassSzPublicStyle::publicFieldArea()
-	const { return publicFieldArea_; }
-inline const Visualization::SequentialLayoutStyle& VClassSzPublicStyle::privateFieldArea()
-	const { return privateFieldArea_; }
-inline const Visualization::SequentialLayoutStyle& VClassSzPublicStyle::protectedFieldArea()
-	const { return protectedFieldArea_; }
-inline const Visualization::SequentialLayoutStyle& VClassSzPublicStyle::defaultFieldArea()
-	const { return defaultFieldArea_; }
 
 }
