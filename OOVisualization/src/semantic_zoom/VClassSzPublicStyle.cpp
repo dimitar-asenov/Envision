c43b1601883e6928d4e1800c3a121cf34403bbbf
patrick-luethi <patrick.luethi@besonet.ch>
2014-02-24 17:39:39 +0100

    Fix copyright year

--- OOVisualization/src/semantic_zoom/VClassSzPublicStyle.cpp
+++ OOVisualization/src/semantic_zoom/VClassSzPublicStyle.cpp
@@ -1,7 +1,7 @@
 
 /***********************************************************************************************************************
 **
-** Copyright (c) 2014, 2016 ETH Zurich
+** Copyright (c) 2011, 2014 ETH Zurich
 ** All rights reserved.
 **
 ** Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
--- OOVisualization/src/semantic_zoom/VMethodSzPublicStyle.cpp
+++ OOVisualization/src/semantic_zoom/VMethodSzPublicStyle.cpp
@@ -1,6 +1,6 @@
 /***********************************************************************************************************************
 **
-** Copyright (c) 2014, 2016 ETH Zurich
+** Copyright (c) 2011, 2014 ETH Zurich
 ** All rights reserved.
 **
 ** Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
