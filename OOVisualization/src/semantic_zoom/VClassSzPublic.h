9ef133dfc388c1d79e67ed9fba704f8e3845f630
patrick-luethi <patrick.luethi@besonet.ch>
2014-02-24 18:37:42 +0100

    Change public interface semantic zoom class visualization to scale itself according to the space available

--- OOVisualization/src/semantic_zoom/VClassSzPublic.h
+++ OOVisualization/src/semantic_zoom/VClassSzPublic.h
@@ -62,8 +62,11 @@ class OOVISUALIZATION_API VClassSzPublic
 		static void initializeForms();
 		int determineForm() override;
 
+		bool sizeDependsOnParent() const;
+
 	protected:
 		void determineChildren() override;
+		virtual void updateGeometry(int availableWidth, int availableHeight) override;
 
 	private:
 		Visualization::Static* icon_{};
