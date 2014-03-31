4e0a7fb85bcc207eb48c294fdc3217be8d25df92
patrick-luethi <patrick.luethi@besonet.ch>
2014-03-31 17:46:34 +0200

    Add declaration visualization

--- OOVisualization/OOVisualization.pro
+++ OOVisualization/OOVisualization.pro
@@ -109,7 +109,9 @@ HEADERS += src/expressions/VNewExpressionStyle.h \
     src/semantic_zoom/VMethodSzPublic.h \
     src/semantic_zoom/VMethodSzPublicStyle.h \
     src/semantic_zoom/VModuleSz.h \
-    src/semantic_zoom/VModuleSzStyle.h
+    src/semantic_zoom/VModuleSzStyle.h \
+    src/semantic_zoom/VDeclarationSz.h \
+    src/semantic_zoom/VDeclarationSzStyle.h
 SOURCES += src/expressions/VNewExpressionStyle.cpp \
     src/expressions/VNewExpression.cpp \
     src/elements/VCommentStatementItem.cpp \
@@ -208,4 +210,6 @@ SOURCES += src/expressions/VNewExpressionStyle.cpp \
     src/semantic_zoom/VMethodSzPublic.cpp \
     src/semantic_zoom/VMethodSzPublicStyle.cpp \
     src/semantic_zoom/VModuleSz.cpp \
-    src/semantic_zoom/VModuleSzStyle.cpp
+    src/semantic_zoom/VModuleSzStyle.cpp \
+    src/semantic_zoom/VDeclarationSz.cpp \
+    src/semantic_zoom/VDeclarationSzStyle.cpp
---
+++ OOVisualization/src/semantic_zoom/VDeclarationSz.cpp
@@ -0,0 +1,77 @@
+/***********************************************************************************************************************
+**
+** Copyright (c) 2011, 2014 ETH Zurich
+** All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+** following conditions are met:
+**
+**    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+**      disclaimer.
+**    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+**      following disclaimer in the documentation and/or other materials provided with the distribution.
+**    * Neither the name of the ETH Zurich nor the names of its contributors may be used to endorse or promote products
+**      derived from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+** INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+** DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+** SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+** WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+***********************************************************************************************************************/
+
+#include "VDeclarationSz.h"
+#include "OOVisualizationException.h"
+
+#include "VisualizationBase/src/items/VText.h"
+#include "VisualizationBase/src/items/Static.h"
+#include "VisualizationBase/src/declarative/DeclarativeItemDef.h"
+
+#include "ModelBase/src/nodes/Node.h"
+
+#include <OOModel/src/declarations/Module.h>
+#include <OOModel/src/declarations/Project.h>
+
+using namespace Visualization;
+using namespace OOModel;
+
+namespace OOVisualization {
+
+ITEM_COMMON_DEFINITIONS(VDeclarationSz, "item")
+
+VDeclarationSz::VDeclarationSz(Item* parent, NodeType* node, const StyleType* style) : Super(parent, node, style)
+{
+	setDefaultMoveCursorProxy(name_);
+}
+
+void VDeclarationSz::initializeForms()
+{
+	auto content = (new GridLayoutFormElement())
+				->setHorizontalSpacing(3)->setColumnStretchFactor(3, 1)
+				->setVerticalAlignment(LayoutStyle::Alignment::Center)
+				->setNoBoundaryCursors([](Item*){return true;})->setNoInnerCursors([](Item*){return true;})
+				->put(0, 0, item<Static>(&I::icon_, [](I* v) -> const Visualization::StaticStyle* {
+						if (DCast<Method>(v->node()))
+							return &v->style()->methodIcon();
+						else if (DCast<Class>(v->node()))
+							return &v->style()->classIcon();
+						else if (DCast<Module>(v->node()))
+							return &v->style()->moduleIcon();
+						else if (DCast<Project>(v->node()))
+							return &v->style()->projectIcon();
+						else
+							Q_ASSERT(false);
+							return nullptr;
+					}))
+				->put(1, 0, item<VText>(&I::name_, [](I* v){return v->node()->nameNode();}, [](I* v) {
+						return &v->style()->name();
+					}));
+
+	addForm(content);
+}
+
+}
