8d8d1b2be0e7caaf1f685cc4bb706007395af089
Patrick Lüthi <patlueth@student.ethz.ch>
2015-07-29 11:20:06 +0200

    Add arguments to MetaDefinition

--- OOModel/src/declarations/MetaDefinition.h
+++ OOModel/src/declarations/MetaDefinition.h
@@ -41,6 +41,7 @@ class OOMODEL_API MetaDefinition : public Super<Declaration>
 {
 	COMPOSITENODE_DECLARE_STANDARD_METHODS(MetaDefinition)
 
+	ATTRIBUTE(Model::TypedList<Declaration>, arguments, setArguments)
 	ATTRIBUTE(Model::TypedList<MetaBinding>, metaBindings, setMetaBindings)
 	ATTRIBUTE(OOModel::Declaration, context, setContext)
 
