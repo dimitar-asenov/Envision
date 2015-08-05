7f6dc1f850edcc2dc4c29eea5d4a18f1daa0a7e8
Lukas Vogel <lukedirtwalker@gmail.com>
2015-08-05 14:13:48 +0200

    Completely refactor CompositeQuery
    -Rename some variables/fields for more clarity
    -Remove add method, it wasn't used anyway
    -Use vector indexes for the input/output mapping

--- InformationScripting/src/queries/CompositeQuery.cpp
+++ InformationScripting/src/queries/CompositeQuery.cpp
@@ -41,63 +41,58 @@ QList<Graph*> CompositeQuery::execute(QList<Graph*> input)
 {
 	inNode_->calculatedOutputs_ = input;
 	// Nodes for which we have all dependencies calculated:
-	QQueue<QueryNode*> workingSetNodes;
-	workingSetNodes.enqueue(inNode_);
-	QSet<QueryNode*> visitedNodes;
+	QQueue<QueryNode*> justExecutedQueries;
+	justExecutedQueries.enqueue(inNode_);
+	QSet<QueryNode*> fullyProcessedQueries;
 
 	// Check if we have nodes which take no input:
 	for (auto queryNode : nodes_)
 	{
-		if (queryNode->inputs_.empty())
+		if (queryNode->inputMap_.empty())
 		{
 			Q_ASSERT(queryNode->q_);
 			queryNode->execute();
-			workingSetNodes.enqueue(queryNode);
+			justExecutedQueries.enqueue(queryNode);
 		}
 	}
 
-	while (!workingSetNodes.empty())
+	while (!justExecutedQueries.empty())
 	{
-		auto currentNode = workingSetNodes.dequeue();
+		auto currentNode = justExecutedQueries.dequeue();
 		// TODO: once we allow cyclic dependencies we have to be smarter here:
-		if (visitedNodes.contains(currentNode)) continue;
-		visitedNodes.insert(currentNode);
+		Q_ASSERT (!fullyProcessedQueries.contains(currentNode));
+		fullyProcessedQueries.insert(currentNode);
 
-		for (auto outputMapping : currentNode->outputs_)
+		for (int outIndex = 0; outIndex < currentNode->outputMap_.size(); ++outIndex)
 		{
-			int outIndex = outputMapping.outputIndex_;
+			auto outputMapping = currentNode->outputMap_[outIndex];
 			// TODO: this shouldn't be an assertion later, but rather some error which is presented to the user.
 			Q_ASSERT(outIndex < currentNode->calculatedOutputs_.size());
 
 			// FIXME: multiple output receivers requires Graph copy constructor:
-			Q_ASSERT(outputMapping.outputReceivers_.size() <= 1);
+			Q_ASSERT(outputMapping.size() <= 1);
 
-			if (outputMapping.outputReceivers_.size() == 1)
+			if (outputMapping.size() == 1)
 			{
-				auto receiver = *outputMapping.outputReceivers_.begin();
+				auto receiver = *outputMapping.begin();
 				// find the input mapping of the receiver:
-				auto inputIt = std::find_if(receiver->inputs_.begin(), receiver->inputs_.end(),
+				auto inputIt = std::find_if(receiver->inputMap_.begin(), receiver->inputMap_.end(),
 					[currentNode, outIndex] (const InputMapping& m) {
 					return m.outputFrom_ == currentNode && m.outputIndex_ == outIndex;
 				});
-				Q_ASSERT(inputIt != receiver->inputs_.end());
-				auto inputMapping = *inputIt;
-				receiver->addCalculatedInput(inputMapping.inputIndex_, currentNode->calculatedOutputs_[outIndex]);
+				Q_ASSERT(inputIt != receiver->inputMap_.end());
+				receiver->addCalculatedInput(std::distance(receiver->inputMap_.begin(), inputIt),
+													  currentNode->calculatedOutputs_[outIndex]);
 
 				if (receiver->canExecute())
 				{
 					receiver->execute();
-					workingSetNodes.enqueue(receiver);
+					justExecutedQueries.enqueue(receiver);
 				}
 			}
 		}
 	}
 	return outNode_->calculatedOutputs_;
-}
-
-void CompositeQuery::addQuery(Query* q)
-{
-	if (!nodeForQuery(q)) nodes_.push_back(new QueryNode(q));
 }
 
 void CompositeQuery::connectQuery(Query* from, Query* to)
@@ -134,29 +129,24 @@ CompositeQuery::QueryNode* CompositeQuery::nodeForQuery(Query* q)
 void CompositeQuery::addOutputMapping(CompositeQuery::QueryNode* outNode, int outIndex,
 												  CompositeQuery::QueryNode* inNode)
 {
-	// Add output mapping
-	auto it = std::find_if(outNode->outputs_.begin(), outNode->outputs_.end(),
-					 [outIndex](const OutputMapping& m) {return m.outputIndex_ == outIndex;});
-	if (it != outNode->outputs_.end())
-		it->outputReceivers_.insert(inNode);
-	else
-		outNode->outputs_.push_back({outIndex, {inNode}});
+	if (outIndex >= outNode->outputMap_.size()) outNode->outputMap_.resize(outIndex + 1);
+	outNode->outputMap_[outIndex].insert(inNode);
 }
 
 void CompositeQuery::addInputMapping(CompositeQuery::QueryNode* outNode, int outIndex,
 												 CompositeQuery::QueryNode* inNode, int inIndex)
 {
-	// Add output mapping
-	auto it = std::find_if(inNode->inputs_.begin(), inNode->inputs_.end(),
-					 [inIndex](const InputMapping& m) {return m.inputIndex_ == inIndex;});
-	if (it != inNode->inputs_.end())
+	if (inIndex >= inNode->inputMap_.size()) inNode->inputMap_.resize(inIndex + 1);
+	auto& existingMapping = inNode->inputMap_[inIndex];
+	if (existingMapping.outputFrom_)
 	{
 		// Only single connection to input allowed:
-		Q_ASSERT(it->outputFrom_ == outNode && it->outputIndex_ == outIndex);
+		Q_ASSERT(existingMapping.outputFrom_ == outNode && existingMapping.outputIndex_ == outIndex);
 		// Already there
 		return;
 	}
-	inNode->inputs_.push_back({inIndex, outNode, outIndex});
+	existingMapping.outputFrom_ = outNode;
+	existingMapping.outputIndex_ = outIndex;
 }
 
 CompositeQuery::QueryNode::~QueryNode()
@@ -164,9 +154,7 @@ CompositeQuery::QueryNode::~QueryNode()
 	// For all calculated outputs check if they are mapped to something, if not delete them:
 	for (int i = 0; i < calculatedOutputs_.size(); ++i)
 	{
-		auto it = std::find_if(outputs_.begin(), outputs_.end(),
-									  [i](const OutputMapping& m) {return m.outputIndex_ == i;});
-		if (it == outputs_.end())
+		if (outputMap_[i].empty())
 			SAFE_DELETE(calculatedOutputs_[i]);
 	}
 	SAFE_DELETE(q_);
@@ -180,15 +168,13 @@ void CompositeQuery::QueryNode::addCalculatedInput(int index, Graph* g)
 	// Insert current input at correct location
 	calculatedInputs_[index] = g;
 	// Set the inserted flag
-	auto it = std::find_if(inputs_.begin(), inputs_.end(),
-								  [index](const InputMapping& m) {return m.inputIndex_ == index;});
-	Q_ASSERT(it != inputs_.end());
-	it->inserted_ = true;
+	Q_ASSERT(index < inputMap_.size() && inputMap_[index].outputFrom_);
+	inputMap_[index].inserted_ = true;
 }
 
 bool CompositeQuery::QueryNode::canExecute() const
 {
-	return std::all_of(inputs_.begin(), inputs_.end(), [](const InputMapping& m) {return m.inserted_;});
+	return std::all_of(inputMap_.begin(), inputMap_.end(), [](const InputMapping& m) {return m.inserted_;});
 }
 
 void CompositeQuery::QueryNode::execute()
--- InformationScripting/src/queries/CompositeQuery.h
+++ InformationScripting/src/queries/CompositeQuery.h
@@ -40,11 +40,6 @@ class INFORMATIONSCRIPTING_API CompositeQuery : public Query
 		virtual QList<Graph*> execute(QList<Graph*> input) override;
 
 		/**
-		 * Adds the Query \a q to this CompositeQuery. This query takes ownership of \a q.
-		 */
-		void addQuery(Query* q);
-
-		/**
 		 * Connects output 0 from Query \a from to input 0 of Query \a to.
 		 *
 		 * If either \a from or \a to was not yet inserted they will be and this query will take ownership of it.
@@ -58,35 +53,32 @@ class INFORMATIONSCRIPTING_API CompositeQuery : public Query
 
 	private:
 		struct QueryNode;
-		/**
-		 * Describes an input mapping:
-		 * The output with index \a outputIndex_ from the Query \a outputFrom_
-		 * is mapped to the input with index \a inputIndex_.
-		 *
-		 * Note: 1 Input can only receive a single output.
-		 */
+
 		struct InputMapping {
-				int inputIndex_{};
 				QueryNode* outputFrom_{};
 				int outputIndex_{};
 				// Indicates whether this output has been calculated and set.
 				bool inserted_{};
 		};
 
-		/**
-		 * Decribes an output mapping:
-		 * 1 Output can go to multiple receivers.
-		 */
-		struct OutputMapping {
-				int outputIndex_{};
-				QSet<QueryNode*> outputReceivers_{};
-		};
-
 		struct QueryNode {
 				QueryNode(Query* q) : q_{q} {}
 				~QueryNode();
-				QList<InputMapping> inputs_;
-				QList<OutputMapping> outputs_;
+
+				/**
+				 * Describes an input mapping:
+				 * The output with index \a outputIndex_ from the Query \a outputFrom_
+				 * is mapped to the input with index i in the vector.
+				 *
+				 * Note: 1 Input can only receive a single output.
+				 */
+				QVector<InputMapping> inputMap_;
+
+				/**
+				 * Decribes an output mapping:
+				 * 1 Output can go to multiple receivers.
+				 */
+				QVector<QSet<QueryNode*>> outputMap_;
 
 				QList<Graph*> calculatedInputs_;
 				QList<Graph*> calculatedOutputs_;
