4e82f724d5385e938e02409cbf266d906864307c
Dimitar Asenov <dimitar.asenov@inf.ethz.ch>
2015-08-25 19:09:58 +0200

    Do not output empty lists in JavaImportTool

--- JavaImportTool/src/javaImportTool/Node.java
+++ JavaImportTool/src/javaImportTool/Node.java
@@ -272,6 +272,10 @@ public class Node {
 			}
 			else if (format_ == OutputFormat.SIMPLE)
 			{
+				// Do not output empty lists
+				if (children_.isEmpty() && (tag_.startsWith("TypedListOf") || tag_.endsWith("List")))
+					return;
+				
 				out_.peek().print(indentation + name_ + " " + tag_ + " {" + id_+"}");
 				assert parent_ != null || !considerPersistenceUnits;
 				out_.peek().print(parent_ == null ? " {00000000-0000-0000-0000-000000000000}": " {" + parent_.id_ + "}");
