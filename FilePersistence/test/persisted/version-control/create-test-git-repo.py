c057067f3358f4db9eef44f387aa6a03605b7b36
Balz Guenat <balz.guenat@gmail.com>
2015-08-26 14:03:44 +0200

    in repo script remove directory before trying to create it

--- FilePersistence/test/persisted/version-control/create-test-git-repo.py
+++ FilePersistence/test/persisted/version-control/create-test-git-repo.py
@@ -44,7 +44,8 @@ files = glob.glob(args.sourceDir + "/*")
 files.sort(compareFileNames)
 
 #Initialize git repo
-rmtree(args.destDir)
+if os.path.exists(args.destDir):
+	rmtree(args.destDir)
 os.mkdir(args.destDir)
 call(['git','init', args.destDir])
 
