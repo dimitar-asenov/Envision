79f0ec1dac16d250d4b4193b56017c4c66d03a61
Lukas Vogel <lukedirtwalker@gmail.com>
2015-09-03 22:41:34 +0200

    Make test script type safe

--- InformationScripting/test/scripts/test.py
+++ InformationScripting/test/scripts/test.py
@@ -1,9 +1,11 @@
 results = [];
 
+import AstApi
 
 for ts in inputs:
     tuples = ts.tuples("ast")
     for tuple in tuples:
         asts = tuple.getAll("ast");
-        print(asts[0].value.constructKind)
+        if type(asts[0].value) is AstApi.Class:
+            print(asts[0].value.constructKind)
     results.append(ts)
