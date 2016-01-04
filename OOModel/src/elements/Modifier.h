bbfa784373a70bd0861a77aadbd85f47d7b57b78
patrick-luethi <patlueth@student.ethz.ch>
2016-01-04 17:46:19 +0100

    add explicit modifier

--- OOModel/src/elements/Modifier.h
+++ OOModel/src/elements/Modifier.h
@@ -57,11 +57,11 @@ class OOMODEL_API Modifier :  public Super<Model::Node>
 			Override = 0x00000080,
 			Inline = 0x00000100,
 			Const = 0x00000200,
+			ConstExpr = 0x0000400,
 
-			Default = 0x00000400,
-			Deleted = 0x00000800,
-
-			ConstExpr = 0x00001000
+			Default = 0x00000800,
+			Deleted = 0x00001000,
+			Explicit = 0x00002000
 		};
 		Q_DECLARE_FLAGS(Modifiers, ModifierFlag)
 
