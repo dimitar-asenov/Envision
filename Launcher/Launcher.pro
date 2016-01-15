d7f5c9c9b1b25fecbc10e03e4fe533ff722e597b
patrick-luethi <patlueth@student.ethz.ch>
2016-01-15 10:45:42 +0100

    removed unused header file

--- Launcher/Launcher.pro
+++ Launcher/Launcher.pro
@@ -1,9 +1,10 @@
 include(../Core/common.pri)
+PRECOMPILED_HEADER =
 TEMPLATE = app
 TARGET = Envision
 target.path = $$BUILD_DIR
 INSTALLS += target
 DEFINES += LAUNCHER
 LIBS += -L$${BUILD_DIR} -lcore
-HEADERS += src/precompiled.h
+HEADERS +=
 SOURCES += src/launcher.cpp
--- Launcher/src/launcher.cpp
+++ Launcher/src/launcher.cpp
@@ -23,7 +23,6 @@
  ** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **
  **********************************************************************************************************************/
-#include "precompiled.h"
 #include "Core/src/core.h"
 
 using namespace Core;
