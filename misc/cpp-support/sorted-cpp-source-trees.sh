207d5a559b00a16544f2491d335aad918445df91
patrick-luethi <patlueth@student.ethz.ch>
2016-02-15 12:10:03 +0100

    process .hpp files

--- misc/cpp-support/sorted-cpp-source-trees.sh
+++ misc/cpp-support/sorted-cpp-source-trees.sh
@@ -54,23 +54,23 @@ cp $CLANG_FORMAT_STYLE $TEMPDIR/
 cp -r $EXPORTED_SOURCE_DIR/* $SORTED_EXPORTED
 
 # Only copy those original files for which there is an equivalent export file
-find $SORTED_EXPORTED \( -name "*.h" -o -name "*.cpp" \) -exec bash -c 'mkdir -p `dirname ${0/$1/$2}`; cp ${0/$1/$3} ${0/$1/$2}' {} "$SORTED_EXPORTED" "$SORTED_ORIGINAL" "$ORIGINAL_SOURCE_DIR" \;
+find $SORTED_EXPORTED \( -name "*.h" -o -name "*.cpp" -o -name "*.hpp" \) -exec bash -c 'mkdir -p `dirname ${0/$1/$2}`; cp ${0/$1/$3} ${0/$1/$2}' {} "$SORTED_EXPORTED" "$SORTED_ORIGINAL" "$ORIGINAL_SOURCE_DIR" \;
 
 ################################################################################
 # Format everything uniformly. It's important to use the provided
 # _clang-format style file as the next step relies on it.
 ################################################################################
 echo "Running clang-format"
-clang-format-3.8 -i -style=file `find $TEMPDIR \( -name "*.h" -o -name "*.cpp" \)`
+clang-format-3.8 -i -style=file `find $TEMPDIR \( -name "*.h" -o -name "*.cpp" -o -name "*.hpp" \)`
 
 ################################################################################
 # Sort "declarations" so that the files are comparable
 ################################################################################
 echo "Sorting 'declarations'"
 # The original sources are sorted without a 3rd argument: all includes will be kept
-find $SORTED_ORIGINAL \( -name "*.h" -o -name "*.cpp" \) -exec bash -c 'python2 $0 $1 $2 &' $SOURCE_SORT_SCRIPT {} {} \;
+find $SORTED_ORIGINAL \( -name "*.h" -o -name "*.cpp" -o -name "*.hpp" \) -exec bash -c 'python2 $0 $1 $2 &' $SOURCE_SORT_SCRIPT {} {} \;
 
 # The exported sources are sorted providing the name of the original source as a third argument.
 # Any includes which only appear in the exported source will be discarded.
 # This is needed since we include all necessary dependencies directly, even if they are already included in other files.
-find $SORTED_EXPORTED \( -name "*.h" -o -name "*.cpp" \) -exec bash -c 'python2 $0 $1 $1 ${1/$2/$3} &' $SOURCE_SORT_SCRIPT {} "$SORTED_EXPORTED" "$SORTED_ORIGINAL" \;
+find $SORTED_EXPORTED \( -name "*.h" -o -name "*.cpp" -o -name "*.hpp" \) -exec bash -c 'python2 $0 $1 $1 ${1/$2/$3} &' $SOURCE_SORT_SCRIPT {} "$SORTED_EXPORTED" "$SORTED_ORIGINAL" \;
