c28a9e8973749376b52257071f00c22fff98328c
Dimitar Asenov <dimitar.asenov@inf.ethz.ch>
2016-03-29 18:28:41 +0200

    Slightly update the cs-why query

--- InformationScripting/scripts/case-studies/associatedBugs.py
+++ InformationScripting/scripts/case-studies/associatedBugs.py
@@ -5,13 +5,16 @@ gh = GitHub()
 repo = gh.repository('dimitar-asenov', 'test-why-use-case')
 
 def referencedIssues(commit):
-    for issueId in re.findall('#(\d+)', commit):
-        yield repo.issue(issueId)
+	for issueId in re.findall('#(\d+)', commit):
+		yield repo.issue(issueId)
 
-for astMsg in Query.input.tuples('astMessage'):
-    msg = '<b>Commit</b><br/>{}<br/>'.format(astMsg.message.replace('\n', '<br/>'))
-    for issue in referencedIssues(astMsg.message):
-        msg += '<b>Fixes #{}</b><br/>{}<br/>'.format(issue.number, issue.title)
-    
-    t = Tuple([('message', msg), ('ast', astMsg.ast), ('type', 'info')])
-    Query.result.add(t)
+for data in Query.input.tuples('data'):
+	text = '<b>Commit</b><br/>{}<br/>'.format(
+				data.message.replace('\n', '<br/>'))
+	for issue in referencedIssues(data.message):
+		text += '<b>Fixes #{}</b><br/>{}<br/>'.format(
+						issue.number, issue.title)
+
+	t = Tuple([ ('message', text),
+			('ast', data.ast), ('type', 'info') ])
+	Query.result.add(t)
