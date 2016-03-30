5f63cc71c25f5772c4d1753d48b3905d12ba0f4c
Manuel Galbier <manuel.galbier@gmail.com>
2016-03-30 13:45:18 +0200

    Test to display the same nodes using two different versions of a project

--- OOInteraction/src/commands/CSceneHandlerItemTest.cpp
+++ OOInteraction/src/commands/CSceneHandlerItemTest.cpp
@@ -29,9 +29,16 @@
 #include "VisualizationBase/src/VisualizationManager.h"
 
 #include "OOModel/src/declarations/Project.h"
+#include "OOModel/src/statements/ExpressionStatement.h"
+#include "OOModel/src/expressions/EmptyExpression.h"
+
 #include "FilePersistence/src/simple/SimpleTextFileStore.h"
+#include "FilePersistence/src/version_control/GitRepository.h"
+
+#include "ModelBase/src/model/AllTreeManagers.h"
 #include "ModelBase/src/model/TreeManager.h"
 #include "ModelBase/src/nodes/Reference.h"
+
 
 namespace OOInteraction {
 
@@ -48,18 +55,95 @@ Interaction::CommandResult* CSceneHandlerItemTest::execute(Visualization::Item*,
 {
 	//Test code goes here
 
-	QString testDir = "projects/";
-	Model::TreeManager* manager = new Model::TreeManager{};
-	FilePersistence::SimpleTextFileStore store;
-	store.setBaseFolder(testDir);
+	QString projectsDir = "projects/";
 
-	//manager->load(&store, "tetris");
-	manager->load(&store, "large", false);
-	auto prj = DCast<OOModel::Project> (manager->root());
+	// versions to compare
+	QString newCommitId = "HEAD";
+	QString oldCommitId = "f1e118bc7e522e2c9404d29d1b21e58956018354";
+
+	// project names
+	QString newVersionProject = "MyTest"+newCommitId;
+	QString oldVersionProject = "MyTest"+oldCommitId;
+
+	FilePersistence::SimpleTextFileStore store;
+	store.setBaseFolder(projectsDir);
+
+	// load newer version
+	Model::TreeManager* newVersionManager = new Model::TreeManager{};
+	newVersionManager->load(&store, newVersionProject, false);
+
+	// load older version
+	Model::TreeManager* oldVersionManager = new Model::TreeManager{};
+	oldVersionManager->load(&store, oldVersionProject, false);
+
 	Model::Reference::resolvePending();
 
-	Visualization::VisualizationManager::instance().mainScene()->addTopLevelNode(prj);
-	Visualization::VisualizationManager::instance().mainScene()->listenToTreeManager(manager);
+	// get GitRepository
+	QString path{projectsDir + newVersionProject};
+	if (!FilePersistence::GitRepository::repositoryExists(path))
+	{
+		qDebug() << "no repository found at " + path;
+		return new Interaction::CommandResult{};
+	}
+
+	FilePersistence::GitRepository repository{path};
+
+	FilePersistence::Diff diff = repository.diff(oldCommitId, newCommitId);
+	auto changes = diff.changes();
+
+	QSet<Model::NodeIdType> changedNodesToVisualize;
+
+	auto typeMatcher = Model::SymbolMatcher::guessMatcher("Class");
+
+	for (auto change : changes.values())
+	{
+		if (change->isFake() || change->onlyStructureChange()) continue;
+
+		auto id = change->nodeId();
+
+		// search one instance of the changed node
+		if (auto node = const_cast<Model::Node*>(Model::AllTreeManagers::instance().nodeForId(id)))
+		{
+
+			Model::Node* changedNode = nullptr;
+
+			if (auto ancestorNode = node->firstAncestorOfType(typeMatcher))
+			{
+				changedNode = ancestorNode;
+				id = newVersionManager->nodeIdMap().id(changedNode);
+			}
+			else
+				changedNode = node;
+
+			// Ignore empty nodes
+			if (auto changedExpr = DCast<OOModel::ExpressionStatement>(changedNode))
+				if (DCast<OOModel::EmptyExpression>(changedExpr->expression()))
+					continue;
+
+			changedNodesToVisualize.insert(id);
+		}
+	}
+
+	Visualization::VisualizationManager::instance().mainScene()->listenToTreeManager(newVersionManager);
+	Visualization::VisualizationManager::instance().mainScene()->listenToTreeManager(oldVersionManager);
+
+	for (auto id : changedNodesToVisualize)
+	{
+		auto oldNode = const_cast<Model::Node*>(oldVersionManager->nodeIdMap().node(id));
+		auto newNode = const_cast<Model::Node*>(newVersionManager->nodeIdMap().node(id));
+
+
+		if (!oldNode)
+			oldNode = new Model::Text{"node in old version not found"};
+
+		Visualization::VisualizationManager::instance().mainScene()->addTopLevelNode(oldNode, 0);
+
+		if (!newNode)
+			newNode = new Model::Text{"node in new version not found"};
+
+		Visualization::VisualizationManager::instance().mainScene()->addTopLevelNode(newNode, 1);
+	}
+
 
 	return new Interaction::CommandResult{};
 }
