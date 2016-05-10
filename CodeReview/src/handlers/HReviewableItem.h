29b386743241b4fec253e37a566f8898f3edd60a
Manuel Galbier <manuel.galbier@gmail.com>
2016-05-10 15:18:33 +0200

    Add basic functionality to comment on selected nodes in DiffView

--- CodeReview/src/handlers/HReviewableItem.h
+++ CodeReview/src/handlers/HReviewableItem.h
@@ -1,6 +1,6 @@
 /***********************************************************************************************************************
  **
- ** Copyright (c) 2011, 2015 ETH Zurich
+ ** Copyright (c) 2011, 2016 ETH Zurich
  ** All rights reserved.
  **
  ** Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
@@ -24,33 +24,20 @@
  **
  **********************************************************************************************************************/
 
-#ifndef PRECOMPILED_CODEREVIEW_H_
-#define PRECOMPILED_CODEREVIEW_H_
+#pragma once
 
-// TODO: Include here the precompiled headers of other plug-ins that this plug-in uses. Only the "public" part of
-// those headers will be included here
-#include "VersionControlUI/src/precompiled.h"
-#include "InteractionBase/src/precompiled.h"
-#include "OOVisualization/src/precompiled.h"
-#include "OOInteraction/src/precompiled.h"
-#include "VisualizationBase/src/precompiled.h"
-#include "OOModel/src/precompiled.h"
-#include "ModelBase/src/precompiled.h"
-#include "Logger/src/precompiled.h"
-#include "SelfTest/src/precompiled.h"
-#include "Core/src/precompiled.h"
-#include "Core/src/global.h"
+#include "../codereview_api.h"
 
+#include "InteractionBase/src/handlers/GenericHandler.h"
 
-// Put here includes which appear in header files. This will also be visible to other plug-in which depend on this one
-// and will be included in their precompiled headers
+namespace CodeReview {
 
+class CODEREVIEW_API HReviewableItem : public Interaction::GenericHandler {
+	protected:
+		HReviewableItem();
 
-#if defined(CodeReview_EXPORTS)
-// Put here includes which only appear in compilation units and do not appear in headers. Precompiled headers of
-// plug-ins which depend on this one will not include these headers.
+	public:
+		static HReviewableItem* instance();
+};
 
-
-#endif
-
-#endif /* PRECOMPILED_CODEREVIEW_H_ */
+}
