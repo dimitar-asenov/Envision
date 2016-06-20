259023c0dcfff19db9c829975cbbaeefe22ca2ec
Dimitar Asenov <dimitar.asenov@inf.ethz.ch>
2016-06-20 19:40:09 +0200

    Only record a modified target for Nodes which have a manager

--- ModelBase/src/commands/NoteNodeChange.cpp
+++ ModelBase/src/commands/NoteNodeChange.cpp
@@ -32,7 +32,8 @@ namespace Model {
 
 NoteNodeChange::NoteNodeChange(QSet<Node*>& modifiedTargets, QSet<Node*>& removedTargets, const UndoCommand* command)
 	: UndoCommand{nullptr, "Note node changes"}, modifiedTargets_{modifiedTargets}, removedTargets_{removedTargets},
-	  target_{command->target()}, insertedNode_{command->insertedNode()}, removedNode_{command->removedNode()}
+		target_{command->target() && command->target()->manager() ? command->target() : nullptr},
+		insertedNode_{command->insertedNode()}, removedNode_{command->removedNode()}
 {
 }
 
