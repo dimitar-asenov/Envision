3450e9b97a2f1b4da4766f8936b831907362e14e
Dimitar Asenov <dimitar.asenov@inf.ethz.ch>
2016-08-03 19:14:54 +0200

    Improve the script for syncing branch IDs
    Now more IDs are matched, as long as they are not in B and look safe to
    match.

--- misc/version-control/scripts/sync_branch_inserted_ids.py
+++ misc/version-control/scripts/sync_branch_inserted_ids.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 # This script is meant to synchronize the IDs of elements that are inserted by both branches, but look like they should be identical.
-# It takes the following arguments in the following order:
-#  - The names of the branchA and branchB files that need to be synced.
-#    The script expects an ID-patch-file of the same name extended with ".idpatch" to exist in the same directory.  This is needed to know what elements from A are also in base.
+# It takes two arguments: the names of the branchA and branchB files that need to be synced.
 #
 # The script will output a patch file that can be used by patch_ids.py in order to patch branchB
 
@@ -23,28 +21,38 @@ with open(args.branchAFilename) as f:
 	branchAList = f.readlines()
 with open(args.branchBFilename) as f:
 	branchBList = f.readlines()
-	
-# Read patch files and extract only ids in base
-with open(args.branchAFilename + patchFileSuffix) as f:
-	idsInBase = {v.rstrip("\n").split(" -> ")[1] for v in  f.readlines()}
-with open(args.branchBFilename + patchFileSuffix) as f:
-	idsInBase.update( {v.rstrip("\n").split(" -> ")[1] for v in  f.readlines()} )
-	
+
 # Compare the files
 transformations = difflib.SequenceMatcher(None, a=branchAList, b=branchBList, autojunk=False).get_opcodes()
 
 # Get replacements of equal size
 replacements = (x for x in transformations if x[0] == 'replace' and (x[2]-x[1] == x[4]-x[3]))
 
-#========================= Helper functions =========================#
-subRegex = re.compile("^(\t*\S+ \S+ \{)(\S+)(\} \{)(\S+)(\}.*)$")
+# Regex to parse Envision files
+subRegex = re.compile("^(\t*)(\S+ \S+ \{)(\S+)(\} \{)(\S+)(\}.*)$")
 
+# Returns a set of ids that are in an Envision file
+def idsInLines( lines ):
+	result = set()
+	for line in lines:
+		match = subRegex.match(line)
+		assert match
+		result.update({match.group(3)})
+	return result
+
+idsInB = idsInLines( branchBList )
+
+# The result will be stored here
+finalMapping = {}
+
+# Returns an ID mapping to convert subListB to subListA, but only if that conversion is safe
 def mappingIfStructureIsIdenticalAndNew( subListA, subListB ):
 	
 	if len(subListA) != len(subListB):
 		return {}
 	
 	result = {}
+	currentRootIndentSize = -1
 	for lineA, lineB in zip(subListA, subListB):
 		matchA = subRegex.match(lineA)
 		matchB = subRegex.match(lineB)
@@ -52,25 +60,34 @@ def mappingIfStructureIsIdenticalAndNew( subListA, subListB ):
 		assert matchB
 		
 		# Only allow identical lines.
-		if matchA.group(1) != matchB.group(1) or matchA.group(5) != matchB.group(5):
-
+		if matchA.group(1) != matchB.group(1) or matchA.group(2) != matchB.group(2) or  matchA.group(6) != matchB.group(6):
+			return {}
+			
+		# Only allow identical IDs, or IDs unsused in B
+		differentId = matchA.group(3) != matchB.group(3)
+		if differentId and ( matchA.group(3) in idsInB ):
 			return {}
 		
-		# Only allow new or identical IDs
-		differentId = matchA.group(2) != matchB.group(2)
-		if differentId and ( matchA.group(2) in idsInBase or matchB.group(2) in idsInBase ):
+		currentIndentSize = len(matchA.group(1))
+		if currentRootIndentSize < 0 or currentIndentSize < currentRootIndentSize:
+			currentRootIndentSize = currentIndentSize
+		
+		# If this line is at the level of the current root, only allow this to work if
+		# the parent node is identical, or will be identical
+		# Otherwise we might end up inserting the same element into two different parents
+		differentParentId = matchA.group(5) != matchB.group(5)
+		if currentIndentSize == currentRootIndentSize and differentParentId and ((matchB.group(5) not in finalMapping) or finalMapping[matchB.group(5)] != matchA.group(5)):
 			return {}
+		
 		
 		# If all checks passed, add this value to the dictionary
 		# It will only be returned if the structures are identical
 		if differentId:
-			result[matchB.group(2)] = matchA.group(2)
+			result[matchB.group(3)] = matchA.group(3)
 	
 	return result
-#========================= End helper functions =========================#
 
 # Get mappings of identical subLists
-finalMapping = {}
 for x in replacements:
 	finalMapping.update(mappingIfStructureIsIdenticalAndNew( branchAList[x[1]:x[2]],  branchBList[x[3]:x[4]]))
 
